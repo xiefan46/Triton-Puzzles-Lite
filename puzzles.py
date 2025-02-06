fa087543e80bf567496ac1a8b391e84fbb0f88d7
xiefan46 <fxie46@gmail.com>
2025-02-06 18:59:24 +0800

    p1

--- puzzles.py
+++ puzzles.py
@@ -216,6 +216,8 @@ def add_kernel(x_ptr, z_ptr, N0, B0: tl.constexpr):
     off_x = tl.arange(0, B0)
     x = tl.load(x_ptr + off_x)
     # Finish me!
+    z = x +10.0
+    tl.store(z_ptr + off_x, z)
     return
 
 
