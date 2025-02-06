923afe9e96bcea9fd14c28b501cacc91644c04c3
xiefan46 <fxie46@gmail.com>
2025-02-06 21:51:51 +0800

    modify

--- puzzles.py
+++ puzzles.py
@@ -346,15 +346,14 @@ def mul_relu_block_kernel(
     x = tl.load(x_ptr + x_offset, mask = x_mask)
     y = tl.load(y_ptr + y_offset, mask = y_mask)
     z = y[:, None] * x[None, :]
+    z = z.reshape
     print(f"z before: {z.shape}")
-    output = tl.max(z, 0)
-    print(f"z after: {output.shape}")
+    relu_z = tl.where(z > 0, z, 0.0)
     z_offset = y_offset[:, None] * N0 + x_offset[None, :]
     z_mask = y_mask[:, None] & x_mask[None, :]
-    print(f"output : {output}")
     print(f"z offset : {z_offset}")
     print(f"z mask: {z_mask}")
-    tl.store(z_ptr + z_offset, output, mask = z_mask)
+    tl.store(z_ptr + z_offset, relu_z, mask = z_mask)
     return
 
 
