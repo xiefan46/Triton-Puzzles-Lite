8d3f252de1c7beb54d33490e88552a5bee0f99de
xiefan46 <fxie46@gmail.com>
2025-02-06 20:10:25 +0800

    modify

--- puzzles.py
+++ puzzles.py
@@ -276,7 +276,7 @@ def add_vec_kernel(x_ptr, y_ptr, z_ptr, N0, N1, B0: tl.constexpr, B1: tl.constex
     print(f"y: {y}")
     z = x + y
     print(f"z: {z}")
-    z_offset = tl.arange(0, B0 * B1)[B1, B0]
+    z_offset = tl.arange(0, B0 * B1)[B1, :]
     tl.store(z_ptr + z_offset, z)
     return
 
