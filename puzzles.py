edadc5c6a0e4a3cbca898baa027f578f87170fc7
xiefan46 <fxie46@gmail.com>
2025-02-06 19:36:59 +0800

    p2

--- puzzles.py
+++ puzzles.py
@@ -241,7 +241,7 @@ def add_mask2_kernel(x_ptr, z_ptr, N0, B0: tl.constexpr):
     # Finish me!
     pid = tl.program_id(0)
     start_pos = pid * B0
-    x_offset = tl.arange(start_pos, start_pos + B0)
+    x_offset = start_pos + tl.arange(0, B0)
     x = tl.load(x_ptr, x_offset, mask = x_offset < N0)
     z = x + 10.0
     tl.store(z_ptr + x_offset, z, mask = x_offset < N0)
