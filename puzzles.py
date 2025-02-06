e904bb983b09266a466508914a9a198d0aa8cfe9
xiefan46 <fxie46@gmail.com>
2025-02-06 21:30:57 +0800

    modify

--- puzzles.py
+++ puzzles.py
@@ -345,8 +345,8 @@ def mul_relu_block_kernel(
     y_mask = y_offset < N1
     x = tl.load(x_ptr + x_offset, mask = x_mask)
     y = tl.load(y_ptr + y_offset, mask = y_mask)
-    zeros = tl.zeros((B1, B0), dtype=tl.float32)
-    z = tl.max(y[:, None] * x[None, :], zeros)
+    z = y[:, None] * x[None, :]
+    z = tl.max(z, 0)
     z_offset = y_offset[:, None] * N0 + x_offset[None, :]
     z_mask = y_mask[:, None] & x_mask[None, :]
     tl.store(z_ptr + z_offset, z, mask = z_mask)
