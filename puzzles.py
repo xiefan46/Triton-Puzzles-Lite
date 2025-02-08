b77fc90cc80cda85a1a482b6e6e5a737ca33382d
xiefan46 <fxie46@gmail.com>
2025-02-08 16:10:31 +0800

    modify

--- puzzles.py
+++ puzzles.py
@@ -539,7 +539,7 @@ def softmax_kernel_brute_force(
         x = tl.load(x_ptr + x_offset, mask=x_mask)
         exp_x = tl.exp2(log2_e * (x - exp_max[:, None]))
         softmax_x = exp_x / exp_sum
-        print(f"softmax_x: {softmax_x}")
+        print(f"softmax_x: {softmax_x}, shape: {softmax_x.shape}")
         tl.store(z_ptr + x_offset, softmax_x, mask=x_mask)
     return
 
