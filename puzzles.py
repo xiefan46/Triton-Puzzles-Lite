e97e67c6b8db5f27e8977dc9acb1213b364f2044
xiefan46 <fxie46@gmail.com>
2025-02-08 17:22:59 +0800

    modify

--- puzzles.py
+++ puzzles.py
@@ -498,7 +498,7 @@ def softmax_kernel(x_ptr, z_ptr, N0, N1, T, B0: tl.constexpr, B1: tl.constexpr):
         off_ij = off_i[:, None] * T + off_j
         mask_ij = mask_i[:, None] & mask_j
         x = tl.load(x_ptr + off_ij, mask=mask_ij)
-        x_max = tl.max(x, axis=1)
+        x_max = tl.max(x, axis=1)[:, None]
         print(f"max shape: {max.shape}, x_max shape: {x_max.shape}")
         exp_x = tl.exp2(log2_e * (x - x_max))
         exp_x_sum = tl.sum(exp_x, axis=1)
